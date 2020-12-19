0eceb2ece97b10d808073297e058e8b97b6a654a
amitdesh <amitdesh2014@gmail.com>
2020-12-19 11:40:36 -0800

    adding batch calls

--- sample.py
+++ sample.py
@@ -22,18 +22,32 @@ pe_ratio = data["PERatio"]
 print(symbol, price, market_cap, pe_ratio)
 
 # Create PD dataframe
-my_columns = ["Ticker", "Stock Price", "Market Cap($bn)", "ForwardPE"]
+my_columns = ["Ticker", "Stock Price", "Market Cap($bn)", "Forward PE"]
 output_dataframe = pd.DataFrame(columns=my_columns)
-output_dataframe.append(
-    pd.Series(
-        [
-            symbol,
-            price,
-            market_cap,
-            pe_ratio
-        ],
-        index=my_columns
-    ),
-    ignore_index=True
-)
-print(output_dataframe)
+data_points = pd.Series(
+    [symbol, price, market_cap, pe_ratio], index=my_columns)
+final_dataframe = output_dataframe.append(data_points, ignore_index=True)
+print(final_dataframe)
+
+output_dataframe = pd.DataFrame(columns=my_columns)
+for ticker in stocks["Ticker"][:5]:
+    api_url = f"https://www.alphavantage.co/query?function=OVERVIEW&symbol={ticker}&interval=15min&slice=year1month1&apikey={config.api_key}"
+    data = requests.get(api_url).json()
+    data_points = pd.Series(
+        [ticker, data['50DayMovingAverage'], data['MarketCapitalization'], data["PERatio"]], index=my_columns)
+    final_dataframe = output_dataframe.append(data_points, ignore_index=True)
+
+print(final_dataframe)
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+    symbol_groups = list(chunks(stocks['Ticker'], 100))
+    symbol_strings = []
+    for i in range(0, len(symbol_groups)):
+        symbol_strings.append(','.join(symbol_groups[i]))
+    #     print(symbol_strings[i])
+    my_columns = ['Ticker', 'Price',
+                  'One-Year Price Return', 'Number of Shares to Buy']
